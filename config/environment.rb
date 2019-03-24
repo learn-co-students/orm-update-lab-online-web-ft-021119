0d208542b16a8aa8c9cc8987a87ac95844b66ba5
lhsharing <daley.luis@gmail.com>
2019-03-23 21:29:27 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,6 @@
 require 'sqlite3'
+require 'pry'
+#require 'pry-nav'
 require_relative '../lib/student'
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
--- lib/student.rb
+++ lib/student.rb
@@ -1,50 +1,81 @@
 require_relative "../config/environment.rb"
-
 class Student
 
-  attr_accessor :name, :grade
-  attr_reader :id
+  attr_accessor :id, :name, :grade
+  #attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-def initialize(id = nil, name, grade)
-  @name = name
-  @grade = grade
-  @id = id
+  def initialize(id = nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+      sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+      SQL
+
+      DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students").flatten.first #[0][0]
+  end
 end
 
-
-def self.create_table
-  
-end
-
-
-def self.drop_table
-  sql = <<-SQL
-  
-  SQL
-end
+  def self.create(name, grade)
+    #binding.pry
+    student_new = Student.new(name, grade)
+    student_new.save
+    student_new
+  end
 
 
-def save
-  
-end
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
+  end
 
 
-def self.create
-  
-end
+  def self.find_by_name(name)
+        sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 
 
-def self.new_from_db
-end
-
-
-def self.find_by_name
-end
-
-
-def update 
-end
+  def update 
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
 
 end
